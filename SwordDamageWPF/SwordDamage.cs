00e456ef07fb67b893539ebdfe77d2ed7d57cb8a
oshan.m <oshan.m@yahoo.com>
2021-07-17 17:02:45 +1000

    Add project files.

---
+++ SwordDamageWPF/SwordDamage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace SwordDamageWPF
+{
+    class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMGE = 2;
+
+        public int Roll;
+        public decimal MagicMultiplier = 1M;
+        public int FlamingDamage = 0;
+        public int Damage;
+
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+            Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll}");
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+            CalculateDamage();
+            Debug.WriteLine($"SetMagic finished: {Damage} (roll: {Roll}");
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMGE;
+            }
+            Debug.WriteLine($"SetFlaming finished: {Damage} (roll: {Roll}");
+        }
+    }
+}
